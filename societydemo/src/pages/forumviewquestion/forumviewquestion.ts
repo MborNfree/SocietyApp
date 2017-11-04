05cc58afdcb6337d65365e44a0ac371471ac37f3
shreyas pednekar <shreyas.pednekar@niyanta.co.in>
2017-11-04 17:54:00 +0530

    Commit

--- societydemo/src/pages/forumviewquestion/forumviewquestion.ts
+++ societydemo/src/pages/forumviewquestion/forumviewquestion.ts
@@ -1,8 +1,7 @@
 import { ForumPage } from './../forum/forum';
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ChatService, ChatMessage, UserInfo } from "../../providers/chat-service";
-import { Events, Content, TextInput } from 'ionic-angular';
+
 
 /**
  * Generated class for the ForumviewquestionPage page.
@@ -19,40 +18,12 @@ import { Events, Content, TextInput } from 'ionic-angular';
 export class ForumviewquestionPage {
 
 
-  showEmojiPicker = false;
-  @ViewChild(Content) content: Content;
-  @ViewChild('chat_input') messageInput: TextInput;
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public chatService: ChatService,
-              public events: Events,) {
+              public navParams: NavParams) {
 
 
   }
-
-  onFocus() {
-    this.showEmojiPicker = false;
-    this.content.resize();
-    this.scrollToBottom();
-}
-
-switchEmojiPicker() {
-    this.showEmojiPicker = !this.showEmojiPicker;
-    if (!this.showEmojiPicker) {
-        this.messageInput.setFocus();
-    }
-    this.content.resize();
-    this.scrollToBottom();
-}
-
-scrollToBottom() {
-  setTimeout(() => {
-      if (this.content.scrollToBottom) {
-          this.content.scrollToBottom();
-      }
-  }, 400)
-}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ForumviewquestionPage');
