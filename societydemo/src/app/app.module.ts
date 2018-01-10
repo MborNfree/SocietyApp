63af83f2f0ab011dd127b6e4c138b4b2beac2ab2
tejaswi pathari <tejaswi.pathari@niyanta.co.in>
2018-01-10 10:47:33 +0530

    no message

--- societydemo/src/app/app.module.ts
+++ societydemo/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PhotosPage } from './../pages/photos/photos';
+import { ImageGalleryPage } from './../pages/image-gallery/image-gallery';
 import { EmailComposer } from "@ionic-native/email-composer";
 import { InAppBrowser } from "@ionic-native/in-app-browser";
 import { BrowserModule } from "@angular/platform-browser";
@@ -122,7 +124,9 @@ var config = {
     ForumviewquestionPage,
     ViewbillPage,
     BalancesheetPage,
-    Card
+    Card,
+    ImageGalleryPage,
+    PhotosPage
     // ThreadCommentsPage,
     // ThreadCreatePage,
     // ThreadsPage,
@@ -180,6 +184,8 @@ var config = {
     ViewbillPage,
     BalancesheetPage,
     Card,
+    ImageGalleryPage,
+    PhotosPage
     // ThreadCommentsPage,
     // ThreadCreatePage,
     // ThreadsPage,
---
+++ societydemo/src/pages/photos/photos.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PhotosPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-photos',
+  templateUrl: 'photos.html',
+})
+export class PhotosPage {
+
+  images = [
+    "holi.jpg",
+    "independence day.png",
+    "Party-Poppers-icon.png",
+    "diwali.jpg"
+  ];
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PhotosPage');
+  }
+
+}
